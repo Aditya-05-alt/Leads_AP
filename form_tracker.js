b6b4ad27e996be7e451d27aa2d760ae096f78907
Aditya-05-alt <113160241+Aditya-05-alt@users.noreply.github.com>
2025-03-31 15:01:55 +0530

    form track

--- form_tracker.js
+++ form_tracker.js
@@ -42,7 +42,7 @@
         }
 
         // 📤 Send to local Django app
-        fetch("http://127.0.0.1:8000/leads/create/", {
+        fetch("https://leadtracker-production.up.railway.app/leads/create/", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
